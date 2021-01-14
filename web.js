559563be147237d2c899f7666bdc69a3535acdf5
TRiG <gerysh07@gmail.com>
2021-01-14 16:44:52 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,6 +1,18 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-app.use(express.static(path.join(process.cwd(), 'source')));
-console.log(process.env.PORT || 5000);
-app.listen(process.env.PORT || 5000);
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    res.writeHead({
+        'Content-Type': 'text/html'
+    })
+
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'source', 'index.js'), (err))
+    }
+    // res.end('<h1>Hello</h1>')
+})
+
+server.listen(5000, () => {
+    console.log('Server start')
+})
