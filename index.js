5ee5116d90be9dec96a9d172df33a2f71e79c2ad
TRiG <gerysh07@gmail.com>
2021-01-14 17:27:07 +0300

    up

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 
 const PORT = process.env.PORT || 80
 
+app.use(express.static(__dirname + '/rwsite/'));
+
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname })
 })
