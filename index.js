1f397c9188b8f16509c96df4f97648736be53847
TRiG <gerysh07@gmail.com>
2021-01-29 17:18:31 +0300

    up

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const PORT = process.env.PORT || 80
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile('slot1.html', { root: __dirname })
+    res.sendFile('slot2-1.html', { root: __dirname })
 })
 
 app.get('/restock.html', (req, res) => {
     res.sendFile('restock.html', { root: __dirname })
+})
+
+app.get('/slot2.html', (req, res) => {
+    res.sendFile('slot2.html', { root: __dirname })
 })
 
 app.listen(PORT, () => {
